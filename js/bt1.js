05fe97d96e97d94e3efbc640cd164dcfa05d0092
maitanhung2 <69668846+maitanhung2@users.noreply.github.com>
2022-12-29 21:34:41 +0700

    hung commit

---
+++ js/bt1.js
@@ -0,0 +1,49 @@
+// in mảng theo thứ tự index tăng dần
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arrNumber.length; i++) {
+//   console.log(arrNumber[i]);
+// }
+
+// in mảng theo thứ tự index giảm dần
+// for (let i = arrNumber.length - 1; i >= 0; i--) {
+//   console.log(arrNumber[i]);
+// }
+// Xóa phần tử cuối cùng của mảng
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arrNumber.pop());
+// console.log(arrNumber);
+
+// Xóa phần tử đầu tiên của mảng
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arrNumber.shift());
+// console.log(arrNumber);
+
+// Thêm phần tử 0 vào cuối mảng
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arrNumber.push(0));
+// console.log(arrNumber);
+
+// Thêm phần tử 0 vào đầu mảng
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arrNumber.unshift(0));
+// console.log(arrNumber);
+
+// Cắt mảng từ phần tử đầu tiên đến phần tử thứ 5 thành 1 mảng mới
+// console.log(arrNumber.splice(0, 5));
+// Chèn thêm số 100 vào phần tử thứ 5 của mảng
+// console.log(arrNumber.splice(4, 0, 100));
+// console.log(arrNumber);
+// Thay thế phần tử thứ 5 bằng số 100
+// console.log(arrNumber.splice(4, 5, 100));
+// console.log(arrNumber);
+
+// copy từ mảng arrNumber thành 1 mảng mới từ phần tử 5 đến hết
+// console.log(arrNumber.splice(5));
+// copy từ mảng arrNumber thành 1 mảng mới từ phần tử đầu tiên đến phần tử thứ 5
+// console.log(arrNumber.splice(0, 5));
+
+// Nối mảng arrNumber2 vào mảng arrNumber
+// var arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var arrNumber2 = [11, 22, 33, 44, 55];
+// console.log(arrNumber.concat(arrNumber2));
